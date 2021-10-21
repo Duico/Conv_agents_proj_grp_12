84bf9af32969df02fc8ecff524e427b2a2b7cb77
Reuben Gardos Reid <ReubenJ@users.noreply.github.com>
2021-10-21 18:14:47 +0200

    Add interrupt state and enable interruptions

--- MathTutor/src/main/kotlin/furhatos/app/mathtutor/flow/general.kt
+++ MathTutor/src/main/kotlin/furhatos/app/mathtutor/flow/general.kt
@@ -1,8 +1,18 @@
 package furhatos.app.mathtutor.flow
 
+import furhatos.app.mathtutor.gaze.Gaze
+import furhatos.app.mathtutor.gaze.getRandomLocation
 import furhatos.flow.kotlin.*
+<<<<<<< HEAD
 import furhatos.util.Gender
 import furhatos.util.Language
+=======
+import furhatos.records.Location
+import furhatos.util.*
+>>>>>>> f2dc7f4 (Add interrupt state and enable interruptions)
+
+val interruptionGaze: Gaze = Gaze("/interrupted.txt")
+val startSpeakingGaze: Gaze = Gaze("/start_speaking.txt")
 
 val Idle: State = state {
 
@@ -25,6 +35,12 @@ val Idle: State = state {
 }
 
 val Interaction: State = state {
+    init {
+        furhat.param.interruptableOnAsk = true                  //to make all states interruptible. This can be done individually for each state as well
+// Make Furhat interruptable during all furhat.say(...)
+        furhat.param.interruptableOnSay = true
+        furhat.param.interruptableWithoutIntents = true          //to make states that implement onResponse { } interruptible as well
+    }
 
     onUserLeave(instant = true) {
         if (users.count > 0) {
@@ -43,7 +59,39 @@ val Interaction: State = state {
         furhat.glance(it)
     }
 
+    onResponse(instant = true, cond = {it.interrupted}) {
+        goto(Interrupted)
+    }
 
+}
+
+val Interrupted: State = state {
+    onEntry {
+        var lookingAway = false
+        val sample = interruptionGaze.getRandomSample()
+
+        if (sample != null) { // Do nothing if, for some reason, the resource file cannot be found
+            for (gazeState in sample) {
+                if (!gazeState) { // Check if we should be looking away (!gazeState)
+                    if (!lookingAway) { // Only find a new spot to look at if furhat is currently looking at the user
+                        // Get some random spot to look at
+                        val absoluteLocation = getRandomLocation()
+                        // Relative to the current user
+                        val relativeLocation = absoluteLocation.add(Location(users.current))
+                        furhat.attend(relativeLocation)
+                        lookingAway = true
+                    }
+                } else {
+                    furhat.attend(users.current)
+                }
+                delay(100) // Sample data is in 100ms buckets, so this loop should only run at that frequency
+            }
+        }
+    }
+
+    onExit {
+        furhat.attend(users.current)
+    }
 }
 
 enum class Operation {
